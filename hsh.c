fc18239ad385c787d2e9210c621c5c916a20769a
thra4974 <tristan.thrasher-1@ou.edu>
2020-04-16 18:40:55 -0500

    removed freeline statement if arrtok is NULL in main

--- hsh.c
+++ hsh.c
@@ -17,26 +17,34 @@ int main(__attribute__((unused)) int argc, char **argv, char **env)
 	char **arrtok;
 	char *PRGM = argv[0];
 	int is_atty = isatty(0);
-	int EXIT_CODE;
+	int EXIT_CODE = 0;
 	size_t n = 0;
 	ssize_t rd = 0;
+	int input_c = 0;
 
-	while (_STAT)
+	do
 	{
 		if (is_atty)
 			_puts("CodeAsIce$ ");
 		rd = getline(&readline, &n, stdin);
 		if (rd == _EOF)
 		{
+			free(readline);
 			exit(EXIT_CODE);
+		}
+		if (readline[input_c] == '\n')
+		{
+			continue;
+			input_c++;
 		}
 		arrtok = tokenize(readline);
 		if (arrtok != NULL)
 		{
 			EXIT_CODE = exec(arrtok, PRGM, readline, env);
-			free(readline);
 			free(arrtok);
 		}
-	}
-	return (0);
+	} while(_STAT);
+	free(readline);
+	free(arrtok);
+	return (EXIT_SUCCESS);
 }
