95a57e8c7b9dae6f61b48e87431d7863e8de6a4d
thra4974 <tristan.thrasher-1@ou.edu>
2020-04-15 23:22:30 -0500

    Satisfied valgrind error msg '144 bytes still reachable'

--- hsh.c
+++ hsh.c
@@ -4,25 +4,30 @@
 
 /**
  * main - main function for holberton shell
+ * @argc: unused parameter
+ * @argv: double pointer to argument vector
+ * @env: dbl pointer to environment vector
  * Return: Exit on success.
  */
 
-int main(void)
+int main(__attribute__((unused)) int argc, char **argv, __attribute__((unused))
+	 char **env)
 {
-	char *read = NULL;
+	char *readline = NULL;
 	char **arrtok = NULL;
 	int is_atty = isatty(0);
+	char *PRGM = argv[0];
 
 	while (_STAT)
 	{
 		if (is_atty)
 			_puts("CodeAsIce$ ");
-		read = readarg();
-		arrtok = tokenize(read);
+		readline = readarg();
+		arrtok = tokenize(readline);
 		if (arrtok != NULL)
 		{
-			exec(arrtok);
-			free(read);
+			exec(arrtok, PRGM, readline);
+			free(readline);
 			free(arrtok);
 		}
 	}
