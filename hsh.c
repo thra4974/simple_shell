6af57a8a9b476f78db486079dda07f6732dc6778
thra4974 <tristan.thrasher-1@ou.edu>
2020-04-16 13:47:56 -0500

    removed readarg.c, added getline to main function in hsh.c

--- hsh.c
+++ hsh.c
@@ -14,16 +14,22 @@
 int main(__attribute__((unused)) int argc, char **argv, char **env)
 {
 	char *readline = NULL;
-	char **arrtok = NULL;
+	char **arrtok;
 	char *PRGM = argv[0];
 	int is_atty = isatty(0);
 	int EXIT_CODE;
+	size_t n = 0;
+	ssize_t rd = 0;
 
 	while (_STAT)
 	{
 		if (is_atty)
 			_puts("CodeAsIce$ ");
-		readline = readarg(EXIT_CODE);
+		rd = getline(&readline, &n, stdin);
+		if (rd == _EOF)
+		{
+			exit(EXIT_CODE);
+		}
 		arrtok = tokenize(readline);
 		if (arrtok != NULL)
 		{
--- splitread.c
+++ splitread.c
@@ -3,21 +3,43 @@
 #define DELM " \n\t\v\r\a"
 
 /**
- * tokenize - splits getline result into vector of strings
+ * _argleng - splits getline result into vector of strings
  * @readline: resulting string form getline
- * Return: array of strings
+ * Return: arg length
+ */
+
+int _argleng(char *readline)
+{
+	int args = 1;
+	int i = 0;
+	int cmd1 = 0;
+
+	while (readline[i] != '\0' && readline[i] != '\n')
+	{
+		if (readline[i] != ' ')
+			cmd1 = 1;
+		if (readline[i] == ' ' && readline[i + 1] != ' '
+		    && readline[i + 1] != '\n' && cmd1 == 1)
+			args++;
+		i++;
+	}
+	return (args);
+}
+
+/**
+ * tokenize - function splits command line input into tokens
+ * @readline: pointer to command line input
+ * Return: double pointer to array of tokens
  */
 
 char **tokenize(char *readline)
 {
-	int len = 0;
 	int i = 0;
-	int bufsize = 0;
+	int bufsize;
+	char **arrtok;
 
-	while (readline[len] != '\0')
-		len++;
-	bufsize = len;
-	char **arrtok = malloc((bufsize + 1) * sizeof(char *));
+	bufsize = _argleng(readline);
+	arrtok = malloc((bufsize + 1) * sizeof(char *));
 
 	if (arrtok == NULL)
 	{
@@ -35,3 +57,9 @@ char **tokenize(char *readline)
 	arrtok[i] = NULL;
 	return (arrtok);
 }
+
+/**
+ * _arglen - counts the number of arguments from user
+ * @readline: pointer to user input arguments
+ * Return: args
+ */
